d607782b90063462ed8d7ce216f35675e9787238
Olga Tolpykina <olga.belevantseva@gmail.com>
2022-05-29 17:50:40 +0300

    feat: add statuc components

--- src/App.vue
+++ src/App.vue
@@ -1,58 +1,30 @@
 <template>
   <div class="container column">
     <resume-form></resume-form>
-
-    <div class="card card-w70">
-      <h1>Резюме Nickname</h1>
-      <div class="avatar">
-        <img
-          src="https://cdn.dribbble.com/users/5592443/screenshots/14279501/drbl_pop_r_m_rick_4x.png"
-        />
-      </div>
-      <h2>Опыт работы</h2>
-      <p>
-        главный герой американского мультсериала «Рик и Морти», гениальный
-        учёный, изобретатель, атеист (хотя в некоторых сериях он даже молится
-        Богу, однако, каждый раз после чудесного спасения ссылается на удачу и
-        вновь отвергает его существование), алкоголик, социопат, дедушка Морти.
-        На момент начала третьего сезона ему 70 лет[1]. Рик боится пиратов, а
-        его главной слабостью является некий - "Санчезиум". Исходя из того, что
-        существует неограниченное количество вселенных, существует
-        неограниченное количество Риков, герой сериала предположительно
-        принадлежит к измерению С-137. В серии комикcов Рик относится к
-        измерению C-132, а в игре «Pocket Mortys» — к измерению C-123[2].
-        Прототипом Рика Санчеза является Эмметт Браун, герой кинотрилогии «Назад
-        в будущее»[3].
-      </p>
-      <h3>Добавьте первый блок, чтобы увидеть результат</h3>
-    </div>
+    <resume-content></resume-content>
   </div>
   <div class="container">
-    <p>
-      <button class="btn primary">Загрузить комментарии</button>
-    </p>
-    <div class="card">
-      <h2>Комментарии</h2>
-      <ul class="list">
-        <li class="list-item">
-          <div>
-            <p><strong>test@microsoft.com</strong></p>
-            <small
-              >Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-              Eligendi, reiciendis.</small
-            >
-          </div>
-        </li>
-      </ul>
-    </div>
-    <div class="loader"></div>
+    <resume-comments></resume-comments>
+    <app-loader></app-loader>
   </div>
 </template>
 
 <script>
 import ResumeForm from "./components/ResumeForm.vue";
+import ResumeContent from "./components/ResumeContent.vue";
+import AppLoader from "./components/AppLoader.vue";
+import ResumeComments from "./components/ResumeComments.vue";
 export default {
-  components: { ResumeForm },
+  components: { ResumeForm, ResumeContent, AppLoader, ResumeComments },
+  methods: {
+    submitForm() {
+      console.group("FormData");
+      console.log("value", this.value);
+      console.log("value", this.type);
+      console.groupEnd();
+      this.value = "";
+    },
+  },
 };
 </script>
 
---
+++ src/components/ResumeContent.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="card card-w70">
+    <resume-title></resume-title>
+    <resume-avatar></resume-avatar>
+    <resume-subtitle></resume-subtitle>
+    <resume-text></resume-text>
+    <h3>Добавьте первый блок, чтобы увидеть результат</h3>
+  </div>
+</template>
+
+<script>
+import ResumeTitle from "./parts/ResumeTitle.vue";
+import ResumeAvatar from "./parts/ResumeAvatar.vue";
+import ResumeSubtitle from "./parts/ResumeSubtitle.vue";
+import ResumeText from "./parts/ResumeText.vue";
+export default {
+  components: { ResumeTitle, ResumeAvatar, ResumeSubtitle, ResumeText },
+};
+</script>
+
+<style scoped></style>
